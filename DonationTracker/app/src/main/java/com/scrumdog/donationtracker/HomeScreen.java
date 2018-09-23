da53700e88607409ebf599ed9cd737231d1f8138
dplalo <deanna.lalo@yahoo.com>
2018-09-23 16:58:56 -0400

    Added login button functionality

--- DonationTracker/app/src/main/java/com/scrumdog/donationtracker/HomeScreen.java
+++ DonationTracker/app/src/main/java/com/scrumdog/donationtracker/HomeScreen.java
@@ -2,6 +2,8 @@ package com.scrumdog.donationtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+    public void login(View view) {
+        // Do something in response to button click
+        startActivity(new Intent(HomeScreen.this, LoginActivity.class));
     }
 }
