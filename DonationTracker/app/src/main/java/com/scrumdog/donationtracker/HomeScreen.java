f11b3d68832dd00867161c9bbf37149314c41a85
dplalo <deanna.lalo@yahoo.com>
2018-09-23 16:24:12 -0400

    Added a homescreen with title and buttons

--- DonationTracker/app/src/main/java/com/scrumdog/donationtracker/HomeScreen.java
+++ DonationTracker/app/src/main/java/com/scrumdog/donationtracker/HomeScreen.java
@@ -1,5 +1,13 @@
 package com.scrumdog.donationtracker;
 
-public class HomeScreen {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
 }
